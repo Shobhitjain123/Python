24baedbdb7876efc6ec123df2919893a21e1eca0
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2025-01-27 13:15:55 +0530

    Repo Setup

---
+++ bin/1_description_comments.py
@@ -0,0 +1,13 @@
+"""
+We can use this for
+description of the code or comments
+"""
+
+'''
+We can also use triple single quotes for 
+descriptin and comments
+'''
+
+# We can use hash for single line comment
+
+print("Hello")
---
+++ bin/3_about_print.py
@@ -0,0 +1,30 @@
+"""
+About 'print' function
+"""
+
+print("print function with 'sep' argument")
+print("-"*20)
+# ---------------
+
+a = 100
+b = 200
+c = 300
+d = 400
+
+#sectionn for sep operator
+print(a, b, c, d) # By Default, In output, it will print each value separated by space
+print(a, b, c, d, sep="\t\t")
+print(a, b, c, d, sep="\n\n")
+print(a, b, c, d, sep="XYZ")
+
+# Section for end operator
+print(a, b, c, d) # Default end="\n", Which means in output, after printing all the values, put \n
+print(a, b, c, d, end="\n\n")
+print(a, b, c, d, end="\n\n")
+print(a, b, c, d, end="XYZ\n")
+
+# We can also pass 'file' parameter to print-function
+# - this we will discuss during file operations
+
+print("#"*40, end="\n\n")
+###########################
